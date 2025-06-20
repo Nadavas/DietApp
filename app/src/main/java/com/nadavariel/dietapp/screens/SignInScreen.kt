2293714cf1796a7fee02e4a76810ee417bab72b5
arielyehuda1 <arielye1234@gmail.com>
2025-06-21 01:58:32 +0300

    small changes in SignInScreen

--- app/src/main/java/com/nadavariel/dietapp/screens/SignInScreen.kt
+++ app/src/main/java/com/nadavariel/dietapp/screens/SignInScreen.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.ClickableText
-import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
